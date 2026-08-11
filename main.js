8a4524405c95ad1ff7440b3e517486335cb3d303
saints-gabriel <gabrieldossantosmartins05@gmail.com>
2026-08-11 16:40:18 -0300

    fix button class

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const but = document.querySelector(".mode")  // Fixed: Select by class instead of ID
+const but = document.querySelector(".mode")
+const link = document.getElementsByTagName('a')
 const gridCont = document.querySelector(".grid-container")
 const gridItem = document.getElementsByClassName(".grid-item")
 
@@ -6,8 +7,10 @@ but.addEventListener('click', () => {
     if (gridCont.style.backgroundColor != "white") {
         gridCont.style.backgroundColor = "white";
         gridCont.classList.add("light-mode");
+        but.classList.add("black");
     } else {
         gridCont.style.backgroundColor = "";
         gridCont.classList.remove("light-mode");
+        but.classList.remove("black");
     }
 });
